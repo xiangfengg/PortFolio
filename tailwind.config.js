f9c8774e305754ddfcfce0cc0f6502510d06b0f9
xiangfengg <74359762+xiangfengg@users.noreply.github.com>
2023-05-29 17:43:35 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: '#f59e0b',
+        dark:'#282828',
+        beach:'#868686',
+        brown:'#565656'
+
+
+
+
+      },
+    },
+  },
+  plugins: [],
+}
